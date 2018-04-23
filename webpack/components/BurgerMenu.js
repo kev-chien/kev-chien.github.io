50297a384d1a0ea1c9c8daa1f251db8f8d170476
Kevin Chien <kevinchien17@gmail.com>
2018-04-23 12:58:05 -0700

    Refactored nav, colored splash, re-colored links, moved about to homepage and rewrote it, fixed Database desc

--- webpack/components/BurgerMenu.js
+++ webpack/components/BurgerMenu.js
@@ -4,7 +4,7 @@ import { slide as Menu } from 'react-burger-menu';
 const BurgerMenu = (props) => {
   return (
     <Menu className = "only-visible-sm" right>
-      <a className="burger-menu-item" href="/about">About</a>
+      <a className="burger-menu-item" href="/">About</a>
       <a className="burger-menu-item" href="/kevin_chien_resume.pdf">Resume</a>
       <a className="burger-menu-item" href="/projects">Projects</a>
     </Menu>
